47f5c7fc48320db92008887494408e60d0e0a166
MatveyYakimov <90453661+MatveyYakimov@users.noreply.github.com>
2021-11-26 01:52:21 +0300

    Add files via upload

---
+++ task 5.py
@@ -0,0 +1,10 @@
+def fibbonachi(n):
+    if n in (1, 2):
+        return 1
+    return fibbonachi(n - 1) + fibbonachi(n - 2)
+
+
+print('Ввдите число: ')
+
+print('Ваше чимло в ряде Финобаччи: ',fibbonachi(int(input())))
+    
